7a91834cab5cc8a0fb192ebccd619a8a04edb61b
Jordi <jordi.bonastre.m@gmail.com>
2021-12-23 18:55:10 +0100

    first commit with boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    ecmaVersion: 13,
+  },
+  ignorePatterns: ["src/test/*", "*.js"],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-console": "off",
+  },
+};
